f7aa8c8485c9cc62f53e15ea99be9d0e76ba8711
QaziAbdulGhafoor <qzgh986@gmail.com>
2025-11-15 13:20:47 +0500

    initial code

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+// mongo db connection
+
+main()
+  .then(() => {
+    console.log("connected to mongo db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+const port = 8080;
+app.listen(port, () => {
+  console.log("listening to port");
+});
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
